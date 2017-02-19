7220aab2bbc56566bbf08f74288613a8a07461fe
fulviocanducci <fulviocanducci@hotmail.com>
2017-02-19 16:13:49 -0300

    NoSQL

---
+++ NoSQL.Library/People.cs
@@ -0,0 +1,9 @@
+﻿namespace NoSQL.Library
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}
